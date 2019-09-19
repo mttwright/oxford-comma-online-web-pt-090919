598872e154cf06275840d72111767e4b24ea658a
virtual-frame-7789 <mttwright@gmail.com>
2019-09-19 03:48:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+  if array.length < 2
+    array.join
+  elsif array.length < 3
+    array.join(" and ")
+  else array.length >= 3
+    new_array = array.collect do |word|
+      word + ", "  
+    end
+    last_word = new_array[-1].split("")
+    last_word.pop
+    last_word.pop
+    last_word.unshift("and ")
+    new_array.pop
+    new_array.push(last_word)
+    new_array.join
+  end
+    
 end
+
+puts oxford_comma(["ham", "bologna", "ciabatta", "milksteak"])
